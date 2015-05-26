df20a579fc34234ce1cc8cb8abab7cea0a1109e3
camiloab01 <camiloab01@gmail.com>
2015-05-26 00:16:38 -0600

    Booking integration and select
    Booking integration and select

--- js/logic.js
+++ js/logic.js
@@ -7,3 +7,9 @@ $(document).ready(function(){
 		interval: 3000 //changes the speed
 	});
 });
+
+(function() {
+	[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {	
+		new SelectFx(el);
+	} );
+})();
